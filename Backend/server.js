6aa29eca61b5efe48dce5a508cf63ddb2ab09773
Ms-venkatraman <m.venkat9626@gmail.com>
2025-12-12 21:12:31 +0530

    update port

--- Backend/server.js
+++ Backend/server.js
@@ -6,13 +6,13 @@ import {connectDB} from './config/constructionDB.js'
 dotenv.config()
 connectDB()
 const app=express()
-const PORT=process.env.PORT||5000
+const PORT=process.env.PORT||10000
 app.use(express.json())
 app.use(cors())
 // http://localhost:3000/api/user
 app.use('/api/user',router)
 
-
+console.log(process.env.MONGO_URI);
 app.listen(PORT,()=>{
     console.log(`server running in http://localhost:${PORT}`)
 })
